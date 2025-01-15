f06b9d17a562fc172785c9b50ea73445c62dbac5
Daniel Asénov <dasenovp01@educantabria.es>
2025-01-15 10:49:45 +0100

    Finished Description Banner

---
+++ Scripts/main-script.js
@@ -0,0 +1,24 @@
+/* FadeIn animation on scroll
+https://stackoverflow.com/questions/27462306/css3-animate-elements-if-visible-in-viewport-page-scroll */
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    document.addEventListener("scroll", function(event) {
+        const animatedBoxes = document.getElementsByClassName("fadeInElement");
+        const windowOffsetTop = window.innerHeight + window.scrollY;
+
+        Array.prototype.forEach.call(animatedBoxes, (animatedBox) => {
+            const animatedBoxOffsetTop = animatedBox.offsetTop;
+
+            if (windowOffsetTop >= animatedBoxOffsetTop) {
+                addClass(animatedBox, "fade-in");
+            }
+        });
+    });
+});
+
+function addClass(element, className) {
+    const arrayClasses = element.className.split(" ");
+    if (arrayClasses.indexOf(className) === -1) {
+        element.className += " " + className;
+    }
+}
